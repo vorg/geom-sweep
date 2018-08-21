8e7dd620f7514a94f753020883d216344b5300f1
Damien Seguin <dmnsgn@gmail.com>
2018-08-21 16:52:12 +0100

    Use pex-context in example

--- example/index.js
+++ example/index.js
@@ -1,43 +1,48 @@
 'use strict'
-const gl = require('pex-gl')()
-const Mat4 = require('pex-math/Mat4')
 const glsl = require('glslify')
+
+const ctx = require('pex-context')()
+const mat4 = require('pex-math/mat4')
+const createCamera = require('pex-cam/perspective')
+const createOrbiter = require('pex-cam/orbiter')
+
 const splitVertices = require('geom-split-vertices')
 const normals = require('geom-normals')
-const camera = require('pex-cam/perspective')({
+const computeNormals = require('angle-normals')
+const triangulate = require('geom-triangulate')
+const splinePoints = require('spline-points')
+
+const sweep = require('../')
+
+const camera = createCamera({
   fov: Math.PI / 3,
-  aspect: gl.canvas.width / gl.canvas.height,
+  aspect: ctx.gl.drawingBufferWidth / ctx.gl.drawingBufferHeight,
   near: 0.1,
   far: 100,
   position: [0, 0, 4]
 })
-require('pex-cam/orbiter')({ camera: camera })
-const sweep = require('../')
-const computeNormals = require('angle-normals')
-const triangulate = require('geom-triangulate')
-const splinePoints = require('spline-points')
-var regl = require('regl')()
+const orbiter = createOrbiter({ camera: camera })
 
-const modelMatrix = Mat4.create()
+const modelMatrix = mat4.create()
 
 let shape = [[-1, -1, 0], [1, -1, 0], [1, 1, 0], [-1, 1, 0]]
 // shape = splinePoints(shape, { segmentLength: 0.1, closed: true })
 // const shape = [
-  // [-1, -1], [0, -1], [0, -0.5], [1, -0.5],
-  // [1, 0.5], [0, 0.5], [0, 1], [-1, 1]
+// [-1, -1], [0, -1], [0, -0.5], [1, -0.5],
+// [1, 0.5], [0, 0.5], [0, 1], [-1, 1]
 // ]
 let path = []
 for (var i = 0; i < 10; i++) {
-  path.push([0.3 * Math.sin(i / 10 * 4 * Math.PI), 2 * (i / 10 - 0.5), 0.3 * Math.cos(i / 10 * 4 * Math.PI)])
+  path.push([0.3 * Math.sin((i / 10) * 4 * Math.PI), 2 * (i / 10 - 0.5), 0.3 * Math.cos((i / 10) * 4 * Math.PI)])
 }
 
 var n = 64
 path.length = 0
 for (var i = 0; i < n; i++) {
-  var t = Math.PI * 2 * i / n
+  var t = (Math.PI * 2 * i) / n
   var x = 10 * (Math.cos(t) + Math.cos(3 * t)) + Math.cos(2 * t) + Math.cos(4 * t)
   var y = 6 * Math.sin(t) + 10 * Math.sin(3 * t)
-  var z = 4 * Math.sin(3 * t) * Math.sin(5 * t / 2) + 4 * Math.sin(4 * t) - 2 * Math.sin(6 * t)
+  var z = 4 * Math.sin(3 * t) * Math.sin((5 * t) / 2) + 4 * Math.sin(4 * t) - 2 * Math.sin(6 * t)
   var s = 1 / 10
   path.push([x * s, y * s, z * s])
 }
@@ -55,115 +60,122 @@ const line = {
   positions: g.debugLines
 }
 
-console.log('g', g)
+// console.log('g', g)
 g = splitVertices(g)
 g.normals = normals(g.positions, g.cells)
 g.uvs = g.normals.map(() => [0, 0])
 
-const drawMesh = regl({
-  attributes: {
-    aPosition: g.positions,
-    // aNormal: g.positions,
-    aNormal: computeNormals(g.cells, g.positions),
-    // aNormal: g.normals,
-    aTexCoord: g.uvs
+const drawMesh = {
+  pipeline: ctx.pipeline({
+    depthTest: true,
+    vert: glsl`
+      #ifdef GL_ES
+      #pragma glslify: transpose = require(glsl-transpose)
+      #endif
+      #pragma glslify: inverse = require(glsl-inverse)
+
+      attribute vec3 aPosition;
+      attribute vec3 aNormal;
+      attribute vec2 aTexCoord;
+
+      uniform mat4 uProjectionMatrix;
+      uniform mat4 uViewMatrix;
+      uniform mat4 uModelMatrix;
+
+      varying vec3 vNormal;
+      varying vec2 vTexCoord;
+
+      void main () {
+        mat4 modelViewMatrix = uViewMatrix * uModelMatrix;
+        mat3 normalMatrix = mat3(transpose(inverse(modelViewMatrix)));
+        vNormal = normalMatrix * aNormal;
+        vTexCoord = aTexCoord;
+        gl_Position = uProjectionMatrix * modelViewMatrix * vec4(aPosition, 1.0);
+      }
+    `,
+    frag: `
+      #ifdef GL_ES
+      precision highp float;
+      #endif
+
+      varying vec3 vNormal;
+      varying vec2 vTexCoord;
+
+      void main () {
+        gl_FragColor.rgb = vNormal * 0.5 + 0.5;
+        // gl_FragColor.rgb = vec3(vTexCoord.xy, 0.0);
+        // gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+        gl_FragColor.a = 1.0;
+      }
+    `
+  }),
+  uniforms: {
+    uProjectionMatrix: camera.projectionMatrix,
+    uViewMatrix: camera.viewMatrix,
+    uModelMatrix: modelMatrix
   },
-  elements: g.cells,
+  attributes: {
+    aPosition: ctx.vertexBuffer(g.positions),
+    // aNormal: ctx.vertexBuffer(g.positions),
+    aNormal: ctx.vertexBuffer(computeNormals(g.cells, g.positions)),
+    // aNormal: ctx.vertexBuffer(g.normals),
+    aTexCoord: ctx.vertexBuffer(g.uvs)
+  },
+  indices: ctx.indexBuffer(g.cells)
   // count: g.positions.length,
   // primitive: 'lines',
-  vert: glsl`
-    #ifdef GL_ES
-    #pragma glslify: transpose = require(glsl-transpose)
-    #endif
-    #pragma glslify: inverse = require(glsl-inverse)
+}
 
-    attribute vec3 aPosition;
-    attribute vec3 aNormal;
-    attribute vec2 aTexCoord;
+const drawLine = {
+  pipeline: ctx.pipeline({
+    vert: glsl`
+      attribute vec3 aPosition;
 
-    uniform mat4 uProjectionMatrix;
-    uniform mat4 uViewMatrix;
-    uniform mat4 uModelMatrix;
+      uniform mat4 uProjectionMatrix;
+      uniform mat4 uViewMatrix;
+      uniform mat4 uModelMatrix;
 
-    varying vec3 vNormal;
-    varying vec2 vTexCoord;
+      void main () {
+        mat4 modelViewMatrix = uViewMatrix * uModelMatrix;
+        gl_Position = uProjectionMatrix * modelViewMatrix * vec4(aPosition, 1.0);
+      }
+    `,
+    frag: `
+      #ifdef GL_ES
+      precision highp float;
+      #endif
 
-    void main () {
-      mat4 modelViewMatrix = uViewMatrix * uModelMatrix;
-      mat3 normalMatrix = mat3(transpose(inverse(modelViewMatrix)));
-      vNormal = normalMatrix * aNormal;
-      vTexCoord = aTexCoord;
-      gl_Position = uProjectionMatrix * modelViewMatrix * vec4(aPosition, 1.0);
-    }
-  `,
-  frag: `
-    #ifdef GL_ES
-    precision highp float;
-    #endif
+      uniform vec4 uColor;
 
-    varying vec3 vNormal;
-    varying vec2 vTexCoord;
-
-    void main () {
-      gl_FragColor.rgb = vNormal * 0.5 + 0.5;
-      // gl_FragColor.rgb = vec3(vTexCoord.xy, 0.0);
-      // gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
-      gl_FragColor.a = 1.0;
-    }
-  `,
-  uniforms: {
-    uProjectionMatrix: () => camera.projectionMatrix,
-    uViewMatrix: () => camera.viewMatrix,
-    uModelMatrix: modelMatrix
-  }
-})
-
-const drawLine = regl({
+      void main () {
+        gl_FragColor.rgb = uColor.rgb;
+        gl_FragColor.a = 1.0;
+      }
+    `,
+    primitive: ctx.Primitive.Lines,
+    depthTest: false
+  }),
   attributes: {
-    aPosition: line.positions
+    aPosition: ctx.vertexBuffer(line.positions)
   },
-  primitive: 'line',
   count: line.positions.length,
-  depth: {
-    enable: false
-  },
-  vert: glsl`
-    attribute vec3 aPosition;
-
-    uniform mat4 uProjectionMatrix;
-    uniform mat4 uViewMatrix;
-    uniform mat4 uModelMatrix;
-
-    void main () {
-      mat4 modelViewMatrix = uViewMatrix * uModelMatrix;
-      gl_Position = uProjectionMatrix * modelViewMatrix * vec4(aPosition, 1.0);
-    }
-  `,
-  frag: `
-    #ifdef GL_ES
-    precision highp float;
-    #endif
-
-    uniform vec4 uColor;
-
-    void main () {
-      gl_FragColor.rgb = uColor.rgb;
-      gl_FragColor.a = 1.0;
-    }
-  `,
   uniforms: {
-    uProjectionMatrix: () => camera.projectionMatrix,
-    uViewMatrix: () => camera.viewMatrix,
-    uModelMatrix: Mat4.create(),
+    uProjectionMatrix: camera.projectionMatrix,
+    uViewMatrix: camera.viewMatrix,
+    uModelMatrix: mat4.create(),
     uColor: [1, 0, 0, 1]
   }
-})
+}
 
-regl.frame(() => {
-  regl.clear({
-    color: [0, 0, 0, 1],
-    depth: 1
+const clearCmd = {
+  pass: ctx.pass({
+    clearColor: [0, 0, 0, 1],
+    clearDepth: 1
   })
-  drawMesh()
-  // drawLine()
+}
+
+ctx.frame(() => {
+  ctx.submit(clearCmd)
+  ctx.submit(drawMesh)
+  // ctx.submit(drawLine)
 })
